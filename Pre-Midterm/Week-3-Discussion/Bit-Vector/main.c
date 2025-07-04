ae88f21e338335d983227bd82c3af0f04029cb7e
Charles <charlesbenedictboquecosa@gmail.com>
2025-07-04 23:24:38 +0800

    bit vector

--- Pre-Midterm/Week-3-Discussion/Bit-Vector/main.c
+++ Pre-Midterm/Week-3-Discussion/Bit-Vector/main.c
@@ -1,2 +1,23 @@
 #include <stdio.h>
 
+#define SIZE 8
+
+int main() {
+    unsigned char bitVector = 0b00000000;
+
+    // Set the 3rd bit (counting from 0)
+    bitVector |= (1 << 3);
+
+    // Check if 3rd bit is set
+    if (bitVector & (1 << 3)) {
+        printf("Bit 3 is set!\n");
+    }
+
+    // Clear the 3rd bit
+    bitVector &= ~(1 << 3);
+
+    // Toggle the 2nd bit
+    bitVector ^= (1 << 2);
+
+    return 0;
+}
