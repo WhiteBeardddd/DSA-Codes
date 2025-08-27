c78c6503ebc4d59ae13521e2dd721919f4824c0e
Charles Benedict <charlesbenedictboquecosa@gmail.com>
2025-08-27 18:49:28 +0800

    helper functions for LinkedList

--- Season_2/Exercises/Exercise_03/linkListFunctions.c
+++ Season_2/Exercises/Exercise_03/linkListFunctions.c
@@ -2,14 +2,51 @@
 #include "functions.h"
 
 //* Helper Functions for ArrayList *//
-void initLList(LinkList *l){
-
+void initLList(LinkList *L){
+    *L = NULL;
 }
-void makeNullLinkedList(LinkList *l){
 
+void makeNullLinkedList(LinkList *L){
+    LinkList temp;
+    while(*L != NULL){
+        temp = *L;
+        *L = (*L)->link;
+        free(temp);
+    }
+    *L = NULL;
 }
-void displayLList(LinkList L){
 
+void displayLList(LinkList L) {
+    if (L != NULL) {
+        printf("\n%-10s %-10s %-5s %-30s\n", "ID", "Course", "Year", "Name");
+        printf("--------------------------------------------------------------\n");
+        while (L != NULL) {
+            printf("%-10s %-10s %-5d %s %c. %s\n",
+                   L->stud.ID,
+                   L->stud.course,
+                   L->stud.yrlevel,
+                   L->stud.name.FN,
+                   L->stud.name.MI,
+                   L->stud.name.LN);
+            L = L->link;  // move to the next node
+        }
+    } else {
+        printf("List is empty\n");
+    }
+}
+
+LinkList createStud(char ID[], char FN[], char LN[], char MI, char course[], int yrlevel){
+    LinkList newNode = malloc(sizeof(struct cell));
+    if(newNode != NULL){
+        newNode->link = NULL;
+        strcpy(newNode->stud.name.FN, FN);
+        strcpy(newNode->stud.name.LN, LN);
+        strcpy(newNode->stud.name.MI, MI);
+        strcpy(newNode->stud.course, course);
+        strcpy(newNode->stud.ID, ID);
+        newNode->stud.yrlevel = yrlevel;
+    }
+    return (newNode != NULL)? newNode: NULL;
 }
 
 //* Main Functions for ArrayList *//
