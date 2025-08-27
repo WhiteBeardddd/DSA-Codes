ec627a24bbb2a4fe49c0b1b64841396e1d65966e
Charles Benedict Boquecosa <charlesbenedictboquecosa@gmail.com>
2025-08-27 17:56:32 +0800

    Code separation

---
+++ Season_2/Exercises/Exercise_03/linkListFunctions.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "functions.h"
+
+//* Helper Functions for ArrayList *//
+void initLList(LinkList *l){
+
+}
+void makeNullLinkedList(LinkList *l){
+
+}
+void displayLList(LinkList L){
+
+}
+
+//* Main Functions for ArrayList *//
+void inserFirstLinkList(LinkList *L, studtype newStud){
+
+}
+
+void inserLastLinkList(LinkList *L, studtype newStud){
+
+}
+void insertLastUniqueLinkList(LinkList *L, studtype newStud){
+
+}
+
+void inserAtPositionLinkList(LinkList *L, studtype newStud, int pos){
+
+}
+
+studtype deleteElemLinkList(LinkList *L, char ID[]){
+
+}
+
+ArrayList separateCourseLinkList(LinkList *L, char course[]){
+    
+}
