9c0ba08d05a9521564da190069dd199d1c85f735
Charles <charlesbenedictboquecosa@gmail.com>
2025-09-18 17:25:25 +0800

    Array ADT version 2

---
+++ 02 - 2nd Take/01 - Arrays/Version - 2/Version - 01/functions.h
@@ -0,0 +1,16 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+#include <stdbool.h>
+#include <stdlib.h>
+#define SIZE 10
+
+// version 2
+typedef struct {
+    char elem[SIZE];
+    int count;
+} *charList;
+
+
+
+
+#endif
--- 02 - 2nd Take/01 - Arrays/Version - 2/Version - 02/main.c
+++ 02 - 2nd Take/01 - Arrays/Version - 2/Version - 02/main.c
@@ -4,7 +4,7 @@
 #include "functions.h"
 
 int main() {
-    charList *list;
+    charList list;
     initList(&list);
 
     // Insert some elements
@@ -13,50 +13,50 @@ int main() {
     insertLast(list, 'C');
     insertLast(list, 'D');
     printf("Original List: ");
-    display(*list);
+    display(list);
 
     // Test insertFirst
     printf("Insert 'X' at First: ");
     insertFirst(list, 'X');
-    display(*list);
+    display(list);
 
     // Test insertMiddle
     printf("Insert 'Y' in Middle: ");
     insertMiddle(list, 'Y');
-    display(*list);
+    display(list);
 
     // Test deleteFirstOccurrence
     printf("Delete first occurrence of 'B': ");
     deleteFirstOccurence(list, 'B');
-    display(*list);
+    display(list);
 
     // Test deleteAllOccurrence
     insertLast(list, 'A'); // Add duplicate 'A'
     insertLast(list, 'A'); // Add another duplicate
     printf("After inserting duplicates of 'A': ");
-    display(*list);
+    display(list);
 
     printf("Delete all occurrences of 'A': ");
     deleteAllOccurence(list, 'A');
-    display(*list);
+    display(list);
 
     // Test deleteLast
     printf("Delete last element: ");
     deleteLast(list);
-    display(*list);
+    display(list);
 
     // Test isExist
-    printf("Is 'C' in the list? %s\n", isExist(*list, 'C') ? "Yes" : "No");
-    printf("Is 'Z' in the list? %s\n", isExist(*list, 'Z') ? "Yes" : "No");
+    printf("Is 'C' in the list? %s\n", isExist(list, 'C') ? "Yes" : "No");
+    printf("Is 'Z' in the list? %s\n", isExist(list, 'Z') ? "Yes" : "No");
 
     // Test locate
-    printf("Locate 'Y': %c\n", locate(*list, 'Y'));
-    printf("Locate 'Z': %c\n", locate(*list, 'Z'));
+    printf("Locate 'Y': %c\n", locate(list, 'Y'));
+    printf("Locate 'Z': %c\n", locate(list, 'Z'));
 
     // Test makenull
     printf("Make null (clear list):\n");
-    makenull(list);
-    display(*list);
+    makeNull(list);
+    display(list);
 
     free(list); // cleanup memory
     return 0;
